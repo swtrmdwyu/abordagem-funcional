a7b950b90b5232c37143e0198349b0ee2c32770b
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-25 16:20:29 -0300

    S05A02 - Promisse race.

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,7 +1,23 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/services/nota-services.js';
-import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js';
+import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js';7
+
+
+// Para caso a promisse demore muito, este é um jeito de fazer com que a execução pare e um erro seja retornado para ser possivel o uso de "TimeOut" ou "try e catch" em promisses.
+const promise1 = new Promise((resolve, reject) => 
+    setTimeout(() => resolve('promise 1 resolvida'), 3000));
+
+const promise2 = new Promise((resolve, reject) => 
+    setTimeout(() => reject('promise 2 resolvida'), 1000));
+
+// Exibirá no console a primeira promisse a ser resolvida.
+Promise.race([ // Recebe as promisses a serem obeservadas.
+    promise1, 
+    promise2 
+])
+.then(console.log) // Mostra a primeira a ser terminada de executar.
+.catch(console.log);
 
 const operations = pipe(
     partialize(takeUntil, 3),
