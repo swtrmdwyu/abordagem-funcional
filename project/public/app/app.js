8a9122490061ffb4e5f8960e53256eaecd78bfbd
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-25 16:13:42 -0300

    S04A06 - Composição do TakeUntil e do DebounceTime.

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,14 +1,19 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/services/nota-services.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js';
 
-const action = debounceTime(500, takeUntil(3, () => //chama o take until se o debounce time tiver sido executado.
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() => 
     service
     .sumItems('2143')
     .then(console.log)
     .catch(console.log)
-));
+);
 
 document
 .querySelector('#myButton')
