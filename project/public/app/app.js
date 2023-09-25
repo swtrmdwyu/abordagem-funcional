b6264915b41f72f912703a9e957b890c4278cb1a
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-25 16:28:55 -0300

    S05A05 - timeout em promisses.

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,23 +1,7 @@
-import { log } from './utils/promise-helpers.js';
+import { log, timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/services/nota-services.js';
 import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js';7
-
-
-// Para caso a promisse demore muito, este é um jeito de fazer com que a execução pare e um erro seja retornado para ser possivel o uso de "TimeOut" ou "try e catch" em promisses.
-const promise1 = new Promise((resolve, reject) => 
-    setTimeout(() => resolve('promise 1 resolvida'), 3000));
-
-const promise2 = new Promise((resolve, reject) => 
-    setTimeout(() => reject('promise 2 resolvida'), 1000));
-
-// Exibirá no console a primeira promisse a ser resolvida.
-Promise.race([ // Recebe as promisses a serem obeservadas.
-    promise1, 
-    promise2 
-])
-.then(console.log) // Mostra a primeira a ser terminada de executar.
-.catch(console.log);
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -25,8 +9,7 @@ const operations = pipe(
 );
 
 const action = operations(() => 
-    service
-    .sumItems('2143')
+    timeoutPromise(200, service.sumItems('2143')) // Se a requisição demorar demais não fara nada
     .then(console.log)
     .catch(console.log)
 );
--- project/public/app/utils/promise-helpers.js
+++ project/public/app/utils/promise-helpers.js
@@ -7,3 +7,17 @@ export const log = (param) => {
     console.log(param);
     return param;
 };
+
+//retorna a promisse que for resolvida ou rejeitada primeiro.
+export const timeoutPromise = (milliseconds, promise) => {
+
+    const timeout =  new Promise((resolve, reject) =>
+        setTimeout(() => 
+            reject(`Limite da operação excedido (limite: ${milliseconds} ms)`), 
+                milliseconds));
+
+    return Promise.race([
+        timeout, 
+        promise
+    ]);
+};
